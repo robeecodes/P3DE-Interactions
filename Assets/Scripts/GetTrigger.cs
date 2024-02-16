f4908c9420bdacd0bae8bf96a55d68d683ca42cf
Robin <97387960+robeecodes@users.noreply.github.com>
2024-02-16 11:59:37 +0000

    Update GetTrigger.cs

--- Assets/Scripts/GetTrigger.cs
+++ Assets/Scripts/GetTrigger.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class GetTrigger : MonoBehaviour
-{
+public class GetTrigger : MonoBehaviour {
+    [SerializeField] private int score;
+    
+    private void Start() {
+        score = 0;
+    }
+
     private void OnTriggerEnter(Collider other) {
-        Debug.Log("Triggered!");
-        Debug.Log(other.gameObject.name);
+        Debug.Log($"Triggered by {other.gameObject.name}");
+        score++;
+        Debug.Log(score);
     }
 }
